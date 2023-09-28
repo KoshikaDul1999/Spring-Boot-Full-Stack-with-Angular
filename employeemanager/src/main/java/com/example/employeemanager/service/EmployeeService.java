c41492e854fccc5e95ab64e903e4454093614599
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-28 17:23:24 +0530

    Create EmployeeService.java

---
+++ employeemanager/src/main/java/com/example/employeemanager/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.example.employeemanager.service;
+
+import com.example.employeemanager.model.Employee;
+import com.example.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+}
