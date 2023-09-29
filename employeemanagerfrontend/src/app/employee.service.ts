34d9b7c807f209b509eeffc42c02dfcb73120ce1
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-29 17:14:47 +0530

    update employee.service.TS

--- employeemanagerfrontend/src/app/employee.service.ts
+++ employeemanagerfrontend/src/app/employee.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Employee } from "./employee";
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +11,19 @@ export class EmployeeService {
 
     constructor(private http: HttpClient) { }
 
-    public getEmployees(): Observable<any> {
-        return this.http.get<any>(`${this.apiServerUrl}/employee/all`);
+    public getEmployees(): Observable<Employee[]> {
+        return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+    }
+
+    public addEmployee(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+    }
+
+    public updateEmployee(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+    }
+
+    public deleteEmployee(employeeId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
     }
 }
