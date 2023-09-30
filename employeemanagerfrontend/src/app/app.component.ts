a76782620e5490f60e7662b68ea5c38ec2a94ac7
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-30 17:14:53 +0530

    update appcomponent.ts

--- employeemanagerfrontend/src/app/app.component.ts
+++ employeemanagerfrontend/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerfrontend';
+export class AppComponent implements OnInit {
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
 }
