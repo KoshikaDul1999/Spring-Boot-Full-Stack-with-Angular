697bc5e7b870ed8f7f429acb7f82f9f670e2cc26
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-30 17:36:15 +0530

    Update appmodule.ts

--- employeemanager/src/main/java/com/example/employeemanager/EmployeemanagerApplication.java
+++ employeemanager/src/main/java/com/example/employeemanager/EmployeemanagerApplication.java
@@ -2,6 +2,7 @@ package com.example.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
@@ -9,5 +10,7 @@ public class EmployeemanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
+
+	@Bean
 
 }
--- employeemanagerfrontend/src/app/app.module.ts
+++ employeemanagerfrontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { EmployeeService } from './employee.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
